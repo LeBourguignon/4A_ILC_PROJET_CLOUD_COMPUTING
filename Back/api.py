838ba1cf0e2dc23eee02ac6ac3dd6804e88a9413
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2023-03-16 18:23:52 +0100

    Update api.py
    retweet

--- Back/api.py
+++ Back/api.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 # from flask_cors import CORS, cross_origin
+
 import json
+import datetime
+import re
 
 app = Flask(__name__)
 # cors = CORS(app)
@@ -8,17 +11,23 @@ app = Flask(__name__)
 users = {}
 
 def login(user, password):
-    return ("u-" + user) in users and users[("u-" + user)] == password
+    return user in users and users[user] == password
 
 class Tweet:
     def __init__(self, author, message):
+        self.date = datetime.datetime.now().isoformat()
         self.author = author    
         self.message = message
+        self.retweet = []
+        self.hashtags = re.findall(r'#\w+', message)
 
     def __json__(self):
         return {
+            "date": self.date,
             "author": self.author,
-            "message": self.message
+            "message": self.message,
+            "retweet": self.retweet,
+            "hashtags": self.hashtags
         }
 
 tweets = []
@@ -31,14 +40,14 @@ def helloWorld():
 @app.route("/seekUser", methods=['POST'])
 def seekUser():
     data = request.get_json()
-    return { "find": ("u-" + data.get('user')) in users }
+    return { "find": data.get('user') in users }
     # curl -X POST -H "Content-Type: application/json" -d '{"user": "Tom"}' http://localhost:5000/seekUser
 
 @app.route("/register", methods=['POST'])
 def registerUser():
     data = request.get_json()
-    if not(("u-" + data.get('user')) in users):
-        users[("u-" + data.get('user'))] = data.get('password')
+    if not(data.get('user') in users):
+        users[data.get('user')] = data.get('password')
         return { "success": True }
     else:
         return { "success": False }
@@ -65,3 +74,17 @@ def newTweet():
     else:
         return { "success": False }
     # curl -X POST -H "Content-Type: application/json" -d '{"user": "Tom", "password": "tomtom", "message": "Ceci est un test !"}' http://localhost:5000/newTweet
+
+@app.route("/retweet", methods=['POST'])
+def retweet():
+    data = request.get_json()
+    dataTweet = data.get('tweet')
+    print(dataTweet)
+    if login(data.get('user'), data.get('password')):
+        for tweet in tweets:
+            print(tweet.__json__())
+            if tweet.__json__() == data.get('tweet'):
+                tweet.retweet.append(data.get('user'))
+                return { "success": True }
+    return { "success": False }
+    # curl -X POST -H "Content-Type: application/json" -d '{"user": "Tom", "password": "tomtom", "tweet": {"author": "Tom", "date": "Thu, 16 Mar 2023 16:57:24 GMT", "hashtags": ["#test", "#first"], "message": "Ceci est un test ! #test #first", "retweet": []}}' http://localhost:5000/retweet
