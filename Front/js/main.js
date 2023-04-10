875d213356eddf00fcfd3395f8af6b697eef1aaf
tom-rh <64775524+tom-rh@users.noreply.github.com>
2023-04-10 14:29:05 +0200

    create a refresh function

--- Front/js/main.js
+++ Front/js/main.js
@@ -1,5 +1,19 @@
-const auteur = document.getElementById(auteur);
+function refresh() {
+	console.log('Refreshing...');
+	// Créer une alerte avec un message personnalisé
+	// https://tailwindcss.com/components/alerts
 
-const heure = document.getElementById(heure);
+	const alert = document.createElement('div');
+	alert.classList.add('bg-blue-500', 'text-white', 'font-bold', 'rounded-t', 'px-4', 'py-3', 'shadow-md', 'mb-2');
+	alert.setAttribute('role', 'alert');
+	alert.innerHTML = 'Refreshing...';
 
-const contenu = document.getElementById(contenu);
+	// Ajouter l'alerte au body
+	const container = document.querySelector('body');
+	container.insertBefore(alert, container.firstChild);
+
+	// Supprimer l'alerte après 2 secondes
+	setTimeout(() => {
+		alert.remove();
+	}, 2000);
+}
